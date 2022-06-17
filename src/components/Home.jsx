f2de7dadc48f3648fa15967e07fd9947866a49c0
Mohit030802 <mohitom2002@gmail.com>
2022-06-18 00:26:41 +0530

    day 2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Mohit Kumar Dubey</h1>
             <h2 className='text-3xl sm:text-5xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
 
-           <p className=' text-[#8892b0]'> I'm a Full stack Developer and a UI/UX designer
+           <p className=' text-[#8892b0] py-4 max-w-[700px]'> I'm a Full stack Developer and a UI/UX designer
                 specializing in building (and occasionally 
                 designing) digital experiences.Currently, I'm focused on 
                 building resposive full stack web application
---
+++ src/components/Skills.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import Javascript from '../assets/javascript.png'
+import ReactImg from '../assets/react.png'
+import Node from '../assets/node.png'
+import Tailwind from '../assets/tailwind.png'
+import GithibImg from '../assets/github.png'
+import Figma from '../assets/figma.svg'
+
+const Skills = () => {
+    return (
+        <div name='skills' className='bg-[#0a192f] w-full h-screen text-gray-300'>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div>
+                    <p className='text-4xl font-bold border-b-4 inline'>Skills</p>
+                    <p className='py-4'>// These are technologies I've worked with</p>
+                </div>
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={HTML} alt="Html icon" />
+                        <p className='my-4'>HTML</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={CSS} alt="Html icon" />
+                        <p className='my-4'>CSS</p>
+                    </div>
+                    
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={Javascript} alt="Html icon" />
+                        <p className='my-4'>JAVASCRIPT</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={ReactImg} alt="Html icon" />
+                        <p className='my-4'>REACT</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={Node} alt="Html icon" />
+                        <p className='my-4'>Node</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={Tailwind} alt="Html icon" />
+                        <p className='my-4'>TAILWIND</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={GithibImg} alt="Html icon" />
+                        <p className='my-4'>GITHUB</p>
+                    </div>
+                    <div className='shadow-md shadow-[#040c16] duration-500 hover:scale-110 '>
+
+                        <img className='w-20 mx-auto' src={Figma} alt="Html icon" />
+                        <p className='my-4'>FIGMA</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Skills
