73ff310a9c629eee446796ea07696fd2cd103e54
Mohit030802 <mohitom2002@gmail.com>
2023-12-05 14:34:52 +0530

    resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -77,7 +77,7 @@ const Navbar = () => {
                     Mail <HiOutlineMail size={30}/></a></li>
                 
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href="https://rose-ninnette-98.tiiny.site/" target="_blank">
+                    <a className='flex justify-between items-center w-full text-gray-300' href="https://drive.google.com/file/d/1eBLxs1u6UbnmuHSlNFzCNcybN6t0-cKj/view?usp=drive_link" target="_blank">
                     Resume <BsFillPersonLinesFill size={30}/></a></li>
             </ul>
         </div>
